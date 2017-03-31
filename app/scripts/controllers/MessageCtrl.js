11acdb778293dbac5e2259453533b062894531b9
Jeffrey C. Charlesworth <jcharlesworth02@gmail.com>
2017-03-31 13:15:09 -0700

    bloc chat 6 complete

--- app/scripts/controllers/MessageCtrl.js
+++ app/scripts/controllers/MessageCtrl.js
@@ -1,18 +1,35 @@
 (function() {
-  function MessageCtrl(Messages, $scope, $firebaseArray, $rootScope) {
+  function MessageCtrl(Messages, $scope, $firebaseArray, $rootScope, $cookies) {
     var ref = firebase.database().ref().child("messages");
 
     $scope.getByRoomId = function (roomId, roomValue) {
+      $rootScope.roomIdHold = roomId;
       $rootScope.currentRoom  = roomValue;
       $rootScope.messagesArray = $firebaseArray(ref.orderByChild("roomId").equalTo(roomId));
       console.log($scope.currentRoom);
       console.log($scope.messagesArray);
     }
-  };
+    $rootScope.send = function(newMessage) {
+      console.log(newMessage);
+      console.log($cookies.get('blocChatCurrentUser'));
+       var newMessageObj = {
+        content: newMessage,
+        roomId: $rootScope.roomIdHold,
+        sentAt: Messages.getTime(),
+        username: $cookies.get('blocChatCurrentUser')
+      }
+      console.log(newMessageObj);
+
+      return $firebaseArray(ref).$add(newMessageObj);
+    };
+
+  }
+
+
 
 
 
   angular
     .module('blocChat')
-    .controller('MessageCtrl', ['Messages', '$scope', '$firebaseArray', '$rootScope', MessageCtrl]);
+    .controller('MessageCtrl', ['Messages', '$scope', '$firebaseArray', '$rootScope', '$cookies', MessageCtrl]);
 })();
--- app/scripts/services/Messages.js
+++ app/scripts/services/Messages.js
@@ -1,7 +1,17 @@
 (function() {
   function Messages($firebaseArray, $rootScope) {
     ref = firebase.database().ref().child("messages");
+    var messageArray = $firebaseArray(ref);
     var Messages = {};
+
+    Messages.getTime = function() {
+      var now = new Date();
+      var date = [now.getMonth() + 1, now.getDate(), now.getFullYear()];
+      var time = [now.getHours(), now.getMinutes(), now.getSeconds()];
+
+      var timestamp = date.join('/') + " " + time.join(":");
+      return timestamp;
+    }
 
 
     return Messages;
